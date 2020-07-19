60c668fa4d9b25ea1719bb17a48ef6983d8e5c24
Kaercher <guilherme.kaercher@gmail.com>
2020-07-18 21:41:56 -0300

    adjust to handle multiple rooms

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000
+
+let lastRoomNumber = 0
+
+const roomsAvailable = [
+  'room0'
+]
+
 server.listen(port, () => {
   console.log(`listening at: ${port}`);
 })
@@ -11,9 +18,35 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+
+  socket.on('player-register', (data) => {
+    const { player } = data
+    const room = roomsAvailable[0]
+    console.log(room)
+    socket.emit('redirect-to-game', { player, room });
+});
+
+    socket.on('join-room', (data) => {
+      const { player, room } = data
+      socket.join(room);
+      const roomDetails = io.sockets.adapter.rooms[room];
+      console.log('room length')
+      console.log(roomDetails.length)
+      if (roomDetails.length === 3) {
+        lastRoomNumber++
+        console.log('lastRoomNumber');
+        console.log(lastRoomNumber)
+        roomsAvailable.shift()
+        roomsAvailable.push(`room${lastRoomNumber}`)
+        io.to(socket.id).emit('update-my-room', { player, room: roomsAvailable[0] })
+        socket.leave(room)
+        socket.join(roomsAvailable[0])
+      }
+    })
+
     socket.on('user-click', (data) => {
-        socket.emit('click-render', data);
-        socket.broadcast.emit('click-render', data);
+      const { room } = data
+        io.in(room).emit('click-render', data)
         console.log(data);
     });
 });
